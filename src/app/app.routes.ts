08df261990398d19def8194aa44f3776fb5fdd37
Ryszard Brzegowy <rbrzegowy@wsei.edu.pl>
2025-12-16 00:34:39 +0100

    routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,12 @@ import { NoteFormComponent } from './components/note-form/note-form.component'
 import { NotesListComponent } from './components/notes-list/notes-list.component'
 import { HomeComponent } from './components/home/home.component'
 import { NotificationListComponent } from './components/notification-list/notification-list.component'
+import { UserLoginComponent } from './components/user-login/user-login.component'
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'list', component: NotesListComponent },
+  { path: 'new', component: NoteFormComponent },
+  { path: 'notifications', component: NotificationListComponent },
+  { path: 'login', component: UserLoginComponent }
 ]
