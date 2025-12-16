715c052a06b206fc32c6360c478f751657aa6415
Ryszard Brzegowy <rbrzegowy@wsei.edu.pl>
2025-12-16 10:43:41 +0100

    added ngrx packages

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,17 @@
 import { ApplicationConfig, provideZoneChangeDetection, isDevMode, LOCALE_ID } from '@angular/core'
 import { provideRouter, withComponentInputBinding } from '@angular/router'
 
-import { routes } from './app.routes'
+import { routes } from './app.routes';
+import { provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { provideEffects } from '@ngrx/effects'
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes, withComponentInputBinding()),
-    { provide: LOCALE_ID, useValue: 'pl' }
-  ]
+    { provide: LOCALE_ID, useValue: 'pl' },
+    provideStore(),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
+    provideEffects()
+]
 }
