bac514face719754d330ffa78dde2d9b2a125e49
Ryszard Brzegowy <rbrzegowy@wsei.edu.pl>
2025-12-16 08:18:16 +0100

    user form

--- src/app/components/user-login/user-login.component.ts
+++ src/app/components/user-login/user-login.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core'
+import { FormsModule } from '@angular/forms'
 
 @Component({
   selector: 'app-user-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-login.component.html',
   styleUrl: './user-login.component.scss'
 })
 export class UserLoginComponent {
-
+  user = {
+    name: '',
+    email: ''
+  }
   onLogin() {
-
+    console.log('User logged in:', this.user)
   }
 }
